233031d81dc8af4eee582f88ae5426761e93cdbd
Hitesh Joshi <hi@hitesh.io>
2020-06-16 02:51:15 +0530

    working pipelines but golang concurrent map read and map write error

--- room.go
+++ room.go
@@ -15,7 +15,6 @@ import (
 	"github.com/pion/rtcp"
 	"github.com/pion/webrtc/v2"
 	"github.com/pion/webrtc/v2/pkg/media"
-	"github.com/rs/xid"
 
 	"github.com/hiteshjoshi/webrtc/gst"
 )
@@ -38,8 +37,8 @@ var (
 	api *webrtc.API
 
 	// Publisher Peer
-	pubCount    int32
-	pubReceiver *webrtc.PeerConnection
+	pubCount int32
+	//pubReceiver *webrtc.PeerConnection
 
 	// Local track
 	videoTrack     *webrtc.Track
@@ -69,32 +68,33 @@ func initGst() {
 	audioMixer, _ := gst.ElementFactoryMake("audiomixer", "audiomixer")
 	audioMixer.SetObject("name", "mix")
 	mixerConvert, _ := gst.ElementFactoryMake("audioconvert", "mixerconvert")
-	audioresample, _ := gst.ElementFactoryMake("audioresample", "audioresample")
+	//audioresample, _ := gst.ElementFactoryMake("audioresample", "audioresample")
 	opusenc, _ := gst.ElementFactoryMake("opusenc", "opusenc")
 	oggmux, _ := gst.ElementFactoryMake("oggmux", "oggmux")
 
-	tee, _ := gst.ElementFactoryMake("tee", "tee")
-	tee.SetObject("name", "tee")
-	audioTrack.AddMany(audioMixer, tee, appsink, mixerConvert, audioresample, opusenc, oggmux)
+	//tee, _ := gst.ElementFactoryMake("tee", "tee")
+	//tee.SetObject("name", "tee")
+
+	audioTrack.AddMany(audioMixer, appsink, mixerConvert, opusenc, oggmux)
 
 	log.Println(audioMixer.Link(mixerConvert))
-	log.Println(mixerConvert.Link(audioresample))
+	log.Println(mixerConvert.Link(opusenc))
 
-	log.Println(audioresample.Link(opusenc))
+	//log.Println(audioresample.Link(opusenc))
 
 	log.Println(opusenc.Link(oggmux))
 	log.Println(oggmux.Link(appsink))
 
-	tee.SetPadAddedCallback(func(element *gst.Element, pad *gst.Pad) {
-		log.Println(pad.Link(audioMixer.GetStaticPad("sink")))
+	//tee.SetPadAddedCallback(func(element *gst.Element, pad *gst.Pad) {
+	//	log.Println(pad.Link(audioMixer.GetStaticPad("sink")))
 
-		//log.Println(pad.Link(usersink.GetStaticPad("sink")))
-		//log.Println(pad.Link(audioMixer.GetRequestPad(audioMixer.GetPadTemplate("src_%u"), "user"+string(atomic.LoadInt32(&pubCount)))))
-	})
+	//log.Println(pad.Link(usersink.GetStaticPad("sink")))
+	//log.Println(pad.Link(audioMixer.GetRequestPad(audioMixer.GetPadTemplate("src_%u"), "user"+string(atomic.LoadInt32(&pubCount)))))
+	//})
 
 	log.Println(audioTrack.Name())
 
-	audioTrack.SetState(gst.StateReady)
+	audioTrack.SetState(gst.StatePlaying)
 }
 
 func room(w http.ResponseWriter, r *http.Request) {
@@ -117,11 +117,12 @@ func room(w http.ResponseWriter, r *http.Request) {
 
 	atomic.AddInt32(&pubCount, 1)
 	userID := fmt.Sprintf("%d", atomic.LoadInt32(&pubCount))
+	var userP *gst.Pipeline
 
 	//atomic.AddInt32(&pubCount, 1)
 
 	// Create a new RTCPeerConnection
-	pubReceiver, err = api.NewPeerConnection(peerConnectionConfig)
+	pubReceiver, err := api.NewPeerConnection(peerConnectionConfig)
 	checkError(err)
 
 	_, err = pubReceiver.AddTransceiver(webrtc.RTPCodecTypeAudio)
@@ -175,9 +176,8 @@ func room(w http.ResponseWriter, r *http.Request) {
 
 		} else {
 			audioTrackLock.Lock()
+			audioTrack.SetState(gst.StateNull)
 			//var err error
-			//guid := xid.New()
-			//uPipe := gst.PipelineNew("user" + guid.String())
 
 			// Create a local audio track, all our SFU clients will be fed via this track
 
@@ -194,15 +194,58 @@ func room(w http.ResponseWriter, r *http.Request) {
 			rtpopusdepay, _ := gst.ElementFactoryMake("rtpopusdepay", "rtpopusdepay"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
 			decodebin, _ := gst.ElementFactoryMake("decodebin", "decodebin"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
 			audioconvert, _ := gst.ElementFactoryMake("audioconvert", "audioconvert"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
-			usersink, _ := gst.ElementFactoryMake("appsink", "appsink"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
-			usersink.SetObject("name", "user"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
+			//usersink, _ := gst.ElementFactoryMake("appsink", "appsink"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
+			//usersink.SetObject("name", "user"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
 
 			// audioTestSrc, _ := gst.ElementFactoryMake("audiotestsrc","testsrcaudio")
 			// testConvert, _ := gst.ElementFactoryMake("audioconvert", "testconvert")
 
-			audioTrack.SetState(gst.StateNull)
-			audioTrack.AddMany(appsrc, capsfilter, queue, rtpopusdepay, decodebin, audioconvert, usersink)
-			//uPipe.AddMany(appsrc,capsfilter,queue,rtpopusdepay,decodebin,audioconvert)
+			//userPipelineAudioConvert, _ := gst.ElementFactoryMake("audioconvert", "userconvert"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
+			//userPipelineopus, _ := gst.ElementFactoryMake("opusenc", "useropus"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
+			//userSink, _ := gst.ElementFactoryMake("appsink", "usersink"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
+
+			//audioTrack.AddMany(userPipelineAudioConvert, userPipelineopus, userSink)
+
+			userP, _ = gst.PipelineNew("user" + fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
+			userAppSrc, _ := gst.ElementFactoryMake("appsrc", "userAppSrc")
+			userAudioConver, _ := gst.ElementFactoryMake("audioconvert", "userAudioConver")
+			userOpus, _ := gst.ElementFactoryMake("opusenc", "userOpus")
+			userShim, _ := gst.ElementFactoryMake("appsink", "userShim")
+			userShim.SetObject("name", "usersink")
+
+			userAppSrc.SetObject("name", "src"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
+			userAppSrc.SetObject("format", gst.NewFormat("time"))
+			userAppSrc.SetObject("is-live", true)
+			userAppSrc.SetObject("do-timestamp", true)
+
+			userCaps, _ := gst.ElementFactoryMake("capsfilter", "usercaps"+userID)
+			userCaps.SetObject("caps", gst.CapsFromString("application/x-rtp, payload=96, encoding-name=OPUS"))
+
+			userQueue, _ := gst.ElementFactoryMake("queue", "userqueue"+userID)
+			useropusdepay, _ := gst.ElementFactoryMake("rtpopusdepay", "userrtpopusdepay"+userID)
+
+			userDecodeBin, _ := gst.ElementFactoryMake("decodebin", "userDecodeBin"+userID)
+
+			//userShim.SetObject("socket-path", "/tmp/user"+fmt.Sprintf("%d", atomic.LoadInt32(&pubCount)))
+			//userShim.SetObject("shm-size", "2000000")
+
+			userP.AddMany(userAppSrc, userAudioConver, useropusdepay, userOpus, userShim, userCaps, userQueue, userDecodeBin)
+
+			log.Println(userAppSrc.Link(userCaps))
+			log.Println(userCaps.Link(userQueue))
+			log.Println(userQueue.Link(useropusdepay))
+
+			log.Println(useropusdepay.Link(userDecodeBin))
+
+			log.Println(userAudioConver.Link(userOpus))
+			log.Println(userOpus.Link(userShim))
+
+			userDecodeBin.SetPadAddedCallback(func(element *gst.Element, pad *gst.Pad) {
+				log.Println(pad.Link(userAudioConver.GetStaticPad("sink")))
+			})
+			userP.SetState(gst.StatePlaying)
+			userPipelines[userID] = userShim
+			audioTrack.AddMany(appsrc, capsfilter, queue, rtpopusdepay, decodebin, audioconvert)
 
 			log.Println("COUNTING FROM HERE")
 			log.Println(appsrc.Link(capsfilter))
@@ -210,15 +253,33 @@ func room(w http.ResponseWriter, r *http.Request) {
 			log.Println(queue.Link(rtpopusdepay))
 			log.Println(rtpopusdepay.Link(decodebin))
 
-			log.Println(audioTrack.Name())
-			tee := audioTrack.GetByName("tee")
-			log.Println(audioconvert.Link(tee)) //Main src to audiomixer
-
-			log.Println("Counting till here")
+			audioMixer := audioTrack.GetByName("mix")
+			log.Println(audioconvert.Link(audioMixer))
 
 			decodebin.SetPadAddedCallback(func(element *gst.Element, pad *gst.Pad) {
-				log.Println(pad.Link(audioconvert.GetStaticPad("sink")))
+				log.Println("audioConvert Pad Link", pad.Link(audioconvert.GetStaticPad("sink")))
+				//log.Println("user Pad Link", pad.Link(userPipelineAudioConvert.GetStaticPad("sink")))
 			})
+
+			//log.Println(userPipelineAudioConvert.Link(userPipelineopus))
+			//log.Println(userPipelineopus.Link(userSink))
+
+			//userPipelines[fmt.Sprintf("%d", atomic.LoadInt32(&pubCount))] = userSink
+
+			//decodebin.SetPadAddedCallback(func(element *gst.Element, pad *gst.Pad) {
+			//	log.Println("decodebin Linking", pad.Link(tee.GetStaticPad("sink")))
+			//})
+
+			//audioTrack.AddMany(audioconvert, tee)
+
+			//log.Println(audioconvert.Link(tee)) //Main src to audiomixer
+
+			//teeTepl := tee.GetPadTemplate("src_%u")
+			//teePad := tee.GetRequestPad(teeTepl, "src_1", nil)
+
+			//fmt.Println("TeePad Linking", audioconvert.GetStaticPad("src").Link(teePad))
+
+			log.Println("Counting till here")
 			audioTrack.SetState(gst.StatePlaying)
 			//src := audioTrack.GetByName("src"+fmt.Sprintf("%d",atomic.LoadInt32(&pubCount)))
 			if err != nil {
@@ -228,12 +289,18 @@ func room(w http.ResponseWriter, r *http.Request) {
 
 			rtpBuf := make([]byte, 1400)
 			audioTrackLock.Unlock()
-			userPipelines[int(atomic.LoadInt32(&pubCount))] = tee
 			for {
 				i, err := remoteTrack.Read(rtpBuf)
-				audioTrackLock.Lock()
-				err = appsrc.PushBuffer(rtpBuf[:i])
-				audioTrackLock.Unlock()
+
+				//err = appsrc.PushBuffer(rtpBuf[:i])
+				for k, v := range userPipelines {
+					if k == userID { //my stream should not have my voice
+						continue
+					}
+					audioTrackLock.Lock()
+					v.PushBuffer(rtpBuf[:i])
+					audioTrackLock.Unlock()
+				}
 
 				if err != io.ErrClosedPipe {
 					checkError(err)
@@ -288,33 +355,20 @@ func room(w http.ResponseWriter, r *http.Request) {
 
 	//npl, _ := gst.ParseLaunch("audiotestsrc ! audioconvert ! appsink name=sink")
 
-	for id, pipeline := range userPipelines {
-		if userID == string(id) {
+	//audioSink := npl.GetByName("sink")
+
+	for {
+		if userPipelines[userID] == nil {
 			continue
 		}
-		pipeline.PushBuffer()
-	}
 
-	nlp, _ := gst.PipelineNew("user")
-
-	testsrc, _ := gst.ElementFactoryMake("audiotestsrc", "testsrcaudio")
-	testopus, _ := gst.ElementFactoryMake("opusenc", "opusenc")
-
-	testsink, _ := gst.ElementFactoryMake("appsink", "sinktest")
-
-	nlp.AddMany(testsrc, testsink, testopus)
-
-	fmt.Println(testsrc.Link(testopus))
-	fmt.Println(testopus.Link(testsink))
-
-	nlp.SetState(gst.StatePlaying)
-	defer func() {
-		nlp.SetState(gst.StateNull)
-		nlp = nil
-	}()
-	//audioSink := npl.GetByName("sink")
-	for {
-		sample, err := testsink.PullSample()
+		if len(userPipelines) < 2 {
+			continue
+		}
+		sink := userP.GetByName("usersink")
+		audioTrackLock.Lock()
+		sample, err := sink.PullSample()
+		audioTrackLock.Unlock()
 
 		//checkError(err)
 		if err != nil {
@@ -331,6 +385,7 @@ func room(w http.ResponseWriter, r *http.Request) {
 		}); err != nil {
 			checkError(err)
 		}
+
 	}
 
 }
